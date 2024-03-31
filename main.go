348b793f727a899501641393c3c5a239c3c02921
OLeg Semashko <olegsemashko@Air-OLeg.lan>
2024-03-31 20:10:16 +0300

    commit1

--- main.go
+++ main.go
@@ -8,13 +8,11 @@ import (
 	"strings"
 )
 
-
-func readLine()string{
+func readLine() string {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	return 	strings.TrimSpace(text)
+	return strings.TrimSpace(text)
 }
-
 
 func main() {
 	db := InitDB("tasks.db")
@@ -26,38 +24,31 @@ func main() {
 		fmt.Println("Какие действия вы хотите произвести с задачами?")
 		fmt.Printf("1 = Добавить новую задачу\n2 = Показать все задачи\n3 = Обновить задачу\n4 = Удалить задачу\n5 = выход\n")
 
-		reader := bufio.NewReader(os.Stdin)
-		choice, _ := reader.ReadString('\n')
-		choice = strings.TrimSpace(choice)
-
-
-		switch choice{
+		choice := readLine()
+		switch choice {
 		case "1":
 			fmt.Println("Ввидите загаловок задачи:")
 			title := readLine()
 
-			
 			fmt.Println("Ввидите описание задачи:")
 			description := readLine()
-
 
 			var category_id int
 			fmt.Println("Ввидите категорию задачи:")
 			fmt.Scan(&category_id)
 
-
 			err := CreateTask(db, title, description, category_id)
-			if err != nil{
+			if err != nil {
 				log.Fatalf("Ошибка добавления задачи: %v", err)
 			}
-				
+
 		case "2":
 			tasks, err := GetAllTasks(db)
-			if err != nil{
+			if err != nil {
 				log.Fatal("Ошибка получения списка задач", err)
 			}
 
-			for _, task := range(tasks){
+			for _, task := range tasks {
 				fmt.Printf("ID: %d, Title: %s, Description: %s, CategoryId: %d, Completed: %t\n", task.ID, task.Title, task.Description, task.CategoryID, task.Completed)
 			}
 
@@ -77,10 +68,9 @@ func main() {
 
 			fmt.Println("Вы хотите отметить ее как выполненую ?: (да/нет)")
 			choice := readLine()
-			var completed bool 
+			var completed bool
 
-
-			switch choice{
+			switch choice {
 			case "да":
 				completed = true
 			case "нет":
@@ -89,16 +79,16 @@ func main() {
 				fmt.Println("Вы ввели неверное значение")
 			}
 
-			if err := UpdateTask(db, id , title, description, category_id, completed); err != nil{
+			if err := UpdateTask(db, id, title, description, category_id, completed); err != nil {
 				log.Fatalf("Ошибка обновления задачи: %v", err)
 			}
-			
+
 		case "4":
 			fmt.Println("Ввидите id задачи, которую вы хотите удалить:")
 			var id int
 			fmt.Scan(&id)
 
-			if err := DeleteTask(db, id); err != nil{
+			if err := DeleteTask(db, id); err != nil {
 				log.Fatalf("Ошибка удаления задачи: %v", err)
 			}
 
@@ -106,7 +96,7 @@ func main() {
 			fmt.Println("Выход из списка задач")
 			return
 		default:
-			fmt.Println("Не верный выбор, попробуйте снова.")	
+			fmt.Println("Не верный выбор, попробуйте снова.")
 		}
 	}
 }
